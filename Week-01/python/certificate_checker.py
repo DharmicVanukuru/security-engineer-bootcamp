eca380ccb7b1c5c382bdd7bd938bcfa4e88e67d8
Dharmic Vanukuru <dharmicv@Dharmics-MacBook-Air.local>
2026-07-25 00:20:37 +0530

    Refactor certificate checker using functions

--- Week-01/python/certificate_checker.py
+++ Week-01/python/certificate_checker.py
@@ -1,14 +1,19 @@
+def print_summary(domain, issuer, expiry_days):
+    print("\nCertificate Summary")
+    print("-------------------")
+    print(f"Domain : {domain}")
+    print(f"Issuer : {issuer}")
+    print(f"Expiry : {expiry_days} days")
+
 domain = input("Enter domain: ")
 issuer = input("Enter issuer: ")
 expiry_days = int(input("Enter the expiry days: "))
 
-print("\nCertificate Summary")
-print("--------------------")
-print("Domain :", domain)
-print("Issuer :", issuer)
-print("Expiry :", expiry_days, "days")
+print_summary(domain, issuer, expiry_days)
 
 if expiry_days < 30:
     print("Status : Warning")
-else:
+elif 30 <= expiry_days <= 90:
+    print("Status : Expiring Soon")
+else :
     print("Status : Valid")
